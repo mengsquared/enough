b31adfc7a4ad660093d2ebb8c05df17433e954ea
Raymond Jacobson <me@raymondjacobson.com>
2014-07-26 21:54:31 -0700

    add ios master & node back

---
+++ ios/Enough/Enough/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Enough
+//
+//  Created by Raymond Jacobson on 7/26/14.
+//  Copyright (c) 2014 raymondjacobson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ ios/Enough/Enough/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Enough
+//
+//  Created by Raymond Jacobson on 7/26/14.
+//  Copyright (c) 2014 raymondjacobson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
