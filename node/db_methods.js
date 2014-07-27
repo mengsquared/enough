2830e9bcc8acbd241d156e5e5371ab318e7934de
Raymond Jacobson <me@raymondjacobson.com>
2014-07-27 15:57:49 -0700

    temp

--- node/db_methods.js
+++ node/db_methods.js
@@ -1,18 +1,50 @@
 var Firebase = require('firebase');
+var Chance = require('chance');
+var chance = new Chance();
+var pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 exports.getDBUrl = function() {
   db = "https://enough-ios-test.firebaseio.com/";
   return db;
 }
+var getDBUrl = function() {
+  db = "https://enough-ios-test.firebaseio.com/";
+  return db;
+}
 
 exports.establishConnection = function(db){
-
   var rootRef = new Firebase(db);
   return rootRef;
 };
+var establishConnection = function(db){
+  var rootRef = new Firebase(db);
+  return rootRef;
+};
+var db_url = getDBUrl();
+var db_root = establishConnection(db_url);
+
+var asyncLoop = function(o){
+    var i=-1,
+        length = o.length;
+    
+    var loop = function(){
+        i++;
+        if(i==length){o.callback(); return;}
+        o.functionToLoop(loop, i);
+    } 
+    loop();//init
+}
 
 exports.pollBlogs = function() {
   setInterval(function(){
-    console.log("POLL");
-  }, 10000);
+    
+  console.log('FIRED');
+  var client = require('twilio')('ACec64a5b0feb7121fd6a33718d1fc4390', '0a4ecac03a0cb832a0cbfd221c329b20');
+  var xml2js = require('xml2js');
+  var string = "http://www.enoughproject.org/blog/feed";
+  var extractedData = "";
+  var parser = new xml2js.Parser();
+
+
+  }, 1000);
 }
--- node/routes.js
+++ node/routes.js
@@ -1,43 +1,74 @@
 var db_methods = require('./db_methods.js');
+var Mailjet = require('./mailjet-sendemail');
+console.log(Mailjet);
+var mailjet = new Mailjet('6ea91449ef465acd2fb58ebf7f409874','632b28c43476eccc99f4f35253cea9d9');
+var unirest = require('unirest');
+
+var db_url = db_methods.getDBUrl();
+var db_root = db_methods.establishConnection(db_url);
 
 exports.article = function(req,res){
-	res.render('article', {
-    postID: req.params['postID'],
-    title: 'Some article about Lorem Ipsum Dolor...',
-    description: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at lectus consequat, venenatis sem vitae, vulputate urna. Nullam sit amet lectus diam. Integer convallis odio id orci bibendum, ut semper dui molestie. Donec ultrices ligula leo, a condimentum urna feugiat eu. Nam pharetra odio eget elit porta, quis scelerisque augue ultrices. Maecenas ullamcorper sed tellus iaculis convallis. Etiam vitae venenatis purus, eget gravida enim. Ut dui arcu, accumsan eu semper porttitor, fringilla ut lectus. Nulla ullamcorper consequat aliquet. Aliquam sollicitudin arcu ac felis dictum semper. Aliquam non magna in mi molestie dapibus vitae vitae quam. Aenean ac diam non elit eleifend consequat ut ac ante. Sed et ullamcorper urna, ut scelerisque nibh. Nunc tempus, urna hendrerit lobortis sagittis, lacus odio vulputate velit, at porttitor turpis massa id nunc.</p><p>Nunc egestas ante eget nisi ultrices luctus a pellentesque purus. Fusce sit amet urna sit amet enim viverra vehicula. Cras erat nisi, convallis sed pharetra luctus, porta vel purus. Quisque quis lacus eu tortor laoreet imperdiet. Mauris suscipit ante sit amet urna sagittis consequat. Suspendisse bibendum elit in iaculis ultrices. Etiam eu lobortis mi. Proin sed eros enim. Cras mollis semper neque id molestie. Nulla ac dignissim est. Suspendisse potenti. Praesent non neque luctus, gravida eros quis, eleifend magna. Donec sit amet molestie risus. Curabitur venenatis bibendum nunc vel tempus.</p>'
-  })
+  var articles_ref = db_root.child("articles").child(req.params['postID']);
+  articles_ref.on('value', function (snapshot) {
+  	res.render('article', {
+      postID: req.params['postID'],
+      title: snapshot.val()['title'],
+      description: snapshot.val()['description']
+    });
+  });
 }
 
 exports.articles = function(req, res){
-  res.render('articles', {
-    title: 'Articles'
-  })
+  var articles_ref = db_root.child("articles")
+  articles_ref.on('value', function (snapshot) {
+    console.log(snapshot.val());
+    res.render('articles', {
+      title: 'Articles',
+      articles: snapshot.val()
+    });
+  });
 }
 
-var mailer = function(user_obj){ 
-  // if(user_obj)
-  if (user_obj['member'] != 'Myself') {
-    if (user_obj['number'] != ''){
-      console.log(user_obj['number']);
-      console.log("SEND TWILIO");
-    }
-    else if (user_obj['email'] != '') {
-      console.log(user_obj['email']);
-      console.log("SEND MAILJET");
-    }
-  }
+var mailer = function(user_obj, post){
+  var articles_ref = db_root.child("articles").child(post);
+  var post_data = {};
+  articles_ref.on('value', function (snapshot) {
+    post_data['title'] = snapshot.val()['title'];
+    post_data['description'] = snapshot.val()['description'];
+    post_data['link'] = snapshot.val()['url'];
+    if (user_obj['member'] != 'Myself') {
+      if (user_obj['number'] != ''){
+        console.log(user_obj['number']);
+        console.log("SEND TWILIO");
+      }
+      else if (user_obj['email'] != '') {
+        console.log(user_obj['email']);
+        e_mailer(user_obj['email'], post_data);
+        console.log("SEND MAILJET");
+      }
+    }  
+  }, function (errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+  });
+}
+
+var e_mailer = function(dest, post) {
+  var title = encodeURI(post['title']);
+  console.log(title);
+  var desc = post['description'];
+  mailjet.sendText('me@raymondjacobson.com',
+    dest, '"Enough Project: '+title.substr(0,30) +'..."', '"'+title +'\n'+'Read the whole story:\n'+post['link']+'"');
+  // mailjet.sendText('me@raymondjacobson.com', 'jacobr2@rpi.edu', 'hi test', 'lol test');
 }
 
 exports.sendmsg = function(req,res){
-  db_url = db_methods.getDBUrl();
-  db_root = db_methods.establishConnection(db_url);
   numbers = [];
   var ph = req.body['phoneNumber'];
   var post = req.body['postID'];
   var userRef = db_root.child("users").child(ph);
   userRef.on('value', function (snapshot) {
     for (var key in snapshot.val()) {
-      mailer(snapshot.val()[key]);
+      mailer(snapshot.val()[key], post);
     }
   }, function (errorObject) {
     console.log('The read failed: ' + errorObject.code);
@@ -46,8 +77,6 @@ exports.sendmsg = function(req,res){
 }
 
 exports.insert = function(req, res){
-	db_url = db_methods.getDBUrl();
-	db_root = db_methods.establishConnection(db_url);
   console.log(req.body);
 	var userRef = db_root.child("users").child(req.body['pilotPhoneNumber']);
   var newNumber = (req.body['mobileNumber']).replace('(','').replace(')','').replace('-','').replace(/\s/g, "");
@@ -65,8 +94,6 @@ exports.insert = function(req, res){
 }
 
 exports.add_phone = function(req, res){
-  db_url = db_methods.getDBUrl();
-  db_root = db_methods.establishConnection(db_url);
   var userRef = db_root.child("users").child(req.body['phoneNumber']);
   userRef.push({
     member: 'Myself',
@@ -84,34 +111,3 @@ exports.testget = function(req, res){
   console.log(req.query);
   res.send('/ GET OK');
 }
-
-// exports.twilio = function(req,res){
-// 	var client = require('twilio')('ACec64a5b0feb7121fd6a33718d1fc4390', '0a4ecac03a0cb832a0cbfd221c329b20');
-// 	//var parseString = require('xml2js').parseString;
-// 	//var xml = "<title>";
-// 	//parseString(xml, function(err, result){
-// 	//	console.log(result);
-// 	//});
-// 	client.sendMessage({
-// 		to: '+2488800325',
-// 		from: '+19513833688',
-// 		body: "hi"
-// 	}, function(err, responseData) { //this function is executed when a response is received from Twilio
-
-//     if (!err) { // "err" is an error received during the request, if any
-
-//         // "responseData" is a JavaScript object containing data received from Twilio.
-//         // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-//         // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
-//         console.log(responseData.from); // outputs "+14506667788"
-//         console.log(responseData.body); // outputs "word to your mother."
-
-//     }
-
-// });
-// // }
-
-// var twil_helper = function(from, item, current_location){
-// 	response_location = location_methods.get_locations();
-// }
