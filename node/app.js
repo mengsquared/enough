8a45365c873e91a521504b09107f1331f224d65b
mengsquared <mengmengjie@Mengmeng-Jies-MacBook-Pro.local>
2014-07-27 05:46:12 -0700

    remove debug

--- node/app.js
+++ node/app.js
@@ -2,13 +2,16 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 
+app.use(express.bodyParser());
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', routes.layout);
 app.get('/add_new_number', routes.addNewNum);
 app.get('/sendmsg', routes.sendmsg);
-app.get('/insert', routes.insert);
+app.post('/insert', routes.insert);
+//app.get('/twilio', routes.twilio);
 
 var server = app.listen(2468, function(){
 	console.log('Listening on port %d', server.address().port);
--- node/db_methods.js
+++ node/db_methods.js
@@ -10,8 +10,3 @@ exports.establishConnection = function(db){
   var rootRef = new Firebase(db);
   return rootRef;
 };
-
-exports.insertTransaction = function(rootRef, sender, member){
-	var transactionsRef = rootRef.child(sender);
-	transactionsRef.set(member);
-}
